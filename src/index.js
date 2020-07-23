1c2f6690510245983e1e17f654890707cac702f3
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2020-07-23 15:25:28 +0100

    accepts arrays of elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+/*
+ *
+ */
+
+
 const addTemplate = createTemplate()
 
 module.exports = function (literals, ...substitutions) {
   const query = createQuery(literals, substitutions)
   if (query === null) return null
-  const elements = Array.from(document.querySelectorAll(query))
-  return elements.length > 0 ? addTemplate(elements) : null
+  if (Array.isArray(query)) {
+    return addTemplate(query)
+  } else {
+    const elements = Array.from(document.querySelectorAll(query))
+    return elements.length > 0 ? addTemplate(elements) : null
+  }
 }
 
 function createTemplate () {
@@ -30,10 +39,15 @@ function addAFunction (customProps, propName, propFunc, funcVars) {
   customProps[propName].value = propFunc(funcVars)
 }
 
+/*
+ * The next functions are all non-enumerable properties given to the created qs objects
+ */
 function eventFunction ({ elements, event }) {
   return function (func) {
     elements.forEach(element => {
-      element.addEventListener(event, func)
+      element.addEventListener(event, (jsEvent) => {
+        func (jsEvent, elements)
+      })
     })
     return elements
   }
@@ -52,20 +66,41 @@ function getFunction ({ elements }) {
   }
 }
 
+/* 
+ * Returns an array of names of all available events
+ */
 function getAllEvents () {
-  return Object.getOwnPropertyNames(document).concat(Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document)))).filter(eventName =>
+  return Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document))).filter(eventName =>
     !eventName.indexOf('on') && (document[eventName] == null || typeof document[eventName] == 'function')
   ).map(eventName => eventName.substr(2))
 }
 
+/*
+ * Takes arguments from a tagged template literal
+ * If valid HTML element array submitted as first argument, return
+ * Else If array entered as a substitution, and no literals, return substitution array
+ * Else If return a query string to be used with document.querySelectorAll
+ * If not valid, return null
+ */
 function createQuery (literals, substitutions) {
-  if (Array.isArray(literals) && Array.isArray(substitutions)) {
-    return literals.map((literal, index) => {
-      return (literal || '') + (substitutions[index] || '')
-    }).join() || null
+  console.log('here', literals, substitutions, substitutions.length, Array.isArray(substitutions))
+  if (Array.isArray(literals) && literals.every(isValidElement)) {
+    return literals
+  } else if (Array.isArray(literals) && Array.isArray(substitutions)) {
+    if (literals.join() === '' && substitutions.every(isValidElement)) {
+      return substitutions
+    } else {
+      return literals.map((literal, index) => {
+        return (literal || '') + (substitutions[index] || '')
+      }).join() || null
+    }
   } else if (typeof literals === 'string' || literals instanceof String) {
     return literals
   } else {
     return null
   }
+}
+
+function isValidElement (element) {
+  return element instanceof Element || element instanceof HTMLDocument
 }
