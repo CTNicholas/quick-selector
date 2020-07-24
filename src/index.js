d5655f18cd42fe00f24bceb318f5fce327f9c731
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2020-07-25 00:17:19 +0100

    comments added, remove listener added

--- src/index.js
+++ src/index.js
@@ -1,58 +1,122 @@
 /*
- *
+ * Initialise events and template
  */
-
-
 const addTemplate = createTemplate()
 
+/*
+ * Exports qs function
+ * Creates querySelectorAll, and return templated result
+ * If element array passed, return templated result
+ */
 module.exports = function (literals, ...substitutions) {
   const query = createQuery(literals, substitutions)
-  if (query === null) return null
+  if (!query || query.length === 0 || query === '[object Object]') return null
   if (Array.isArray(query)) {
     return addTemplate(query)
   } else {
     const elements = Array.from(document.querySelectorAll(query))
-    return elements.length > 0 ? addTemplate(elements) : null
+    return elements.length > 0 ? addTemplate(elements) : []
   }
 }
 
+/*
+ * Initialises events and customMethods, then returns a function that applies
+ * changes to an element list:
+
+ * Each of these properties applies to every element in the array:
+ * - Adds every type of eventListener to the qs element list's hidden properties
+ *   qs`.element`.click(clickFunc)
+ * 
+ * - Adds a .remove property, to remove eventListeners 
+ *   qs`.element`.remove.click(clickFunc)
+ * 
+ * - Adds set, to set properties of elements
+ *   qs`.element`.set(el => el.style.color = 'red')
+ * 
+ * - Adds get, to get an array of properties of elements
+ *   const getText = qs`.element`.get(el => el.innerHTML) // ['an element', 'inside another']
+ * 
+ * - Adds only to select a single element from an index, returning an array of 1
+ *   qs`.element`.only(1) // [ HTMLElement ]
+ * 
+ * - Modifies the following array methods, to return an array that has been parsed by qs:
+ *   Filter
+ *   qs`.element`.filter(el => el.innerHTML.includes('Title'))
+ */
 function createTemplate () {
   const events = getAllEvents()
   const customMethods = {
     set: setFunction,
-    get: getFunction
+    get: getFunction,
+    only: onlyFunction,
+    filter: arrayFunction
   }
   return function (elements) {
     const customProps = {}
+    const removeEventObject = {}
     events.forEach(event => {
       addAFunction(customProps, event, eventFunction, { elements, event })
+      removeEventObject[event] = removeEventFunction({ elements, event })
     })
+    customProps.remove = { value: removeEventObject }
     Object.entries(customMethods).forEach(([methodName, methodFunc]) => {
-      addAFunction(customProps, methodName, methodFunc, { elements })
+      addAFunction(customProps, methodName, methodFunc, { elements, methodName })
     })
     return Object.defineProperties(elements, customProps)
   }
 }
 
+/*
+ * Adds a function to customProps, to be used in defuneProperties
+ */
 function addAFunction (customProps, propName, propFunc, funcVars) {
-  customProps[propName] = {}
-  customProps[propName].value = propFunc(funcVars)
+  customProps[propName] = {
+    value: propFunc(funcVars)
+  }
 }
 
 /*
- * The next functions are all non-enumerable properties given to the created qs objects
+ * The next few functions are all non-enumerable properties given to the created qs objects
+ * Or in one case, an object that enables removes event listeners
+ * 
+ * Creates an event listener, returns elements
  */
 function eventFunction ({ elements, event }) {
   return function (func) {
     elements.forEach(element => {
-      element.addEventListener(event, (jsEvent) => {
-        func (jsEvent, elements)
-      })
+      element.addEventListener(event, func)// (jsEvent) => {
+      //  func (jsEvent, elements)
+      //})
     })
     return elements
   }
 }
 
+/*
+ * Removes event listener, returns elements
+ */
+function removeEventFunction ({ elements, event }) {
+  return function (func) {
+    console.log('Removing', event, elements)
+    elements.forEach(element => {
+      element.removeEventListener(event, func)
+    })
+    return elements
+  }
+}
+
+/*
+ * Applies array method then returns qs templated array
+ */
+function arrayFunction ({ elements, methodName }) {
+  return function (func) {
+    return addTemplate([][methodName].call(elements, func))
+  }
+}
+
+/*
+ * Cycles through elements letting you edit elements
+ */
 function setFunction ({ elements }) {
   return function (func) {
     elements.forEach(func)
@@ -60,19 +124,33 @@ function setFunction ({ elements }) {
   }
 }
 
+/*
+ * Returns elements selected in func
+ */
 function getFunction ({ elements }) {
   return function (func) {
     return elements.map(element => func(element))
   }
 }
 
+/*
+ * Selects an element by index and returns a qs templated array containing it
+ */
+function onlyFunction ({ elements }) {
+  return function (index) {
+    return addTemplate([elements[index]])
+  }
+}
+
 /* 
  * Returns an array of names of all available events
+ * Will always find a list of events, the second return value is for testing
  */
 function getAllEvents () {
-  return Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document))).filter(eventName =>
+  const allEvents = Object.getOwnPropertyNames(Object.getPrototypeOf(Object.getPrototypeOf(document))).filter(eventName =>
     !eventName.indexOf('on') && (document[eventName] == null || typeof document[eventName] == 'function')
   ).map(eventName => eventName.substr(2))
+  return allEvents.length > 0 ? allEvents : ['click', 'mouseover']
 }
 
 /*
@@ -83,7 +161,6 @@ function getAllEvents () {
  * If not valid, return null
  */
 function createQuery (literals, substitutions) {
-  console.log('here', literals, substitutions, substitutions.length, Array.isArray(substitutions))
   if (Array.isArray(literals) && literals.every(isValidElement)) {
     return literals
   } else if (Array.isArray(literals) && Array.isArray(substitutions)) {
@@ -92,7 +169,7 @@ function createQuery (literals, substitutions) {
     } else {
       return literals.map((literal, index) => {
         return (literal || '') + (substitutions[index] || '')
-      }).join() || null
+      }).join('') || null
     }
   } else if (typeof literals === 'string' || literals instanceof String) {
     return literals
@@ -101,6 +178,9 @@ function createQuery (literals, substitutions) {
   }
 }
 
+/*
+ *  Check if element is valid DOM element
+ */
 function isValidElement (element) {
   return element instanceof Element || element instanceof HTMLDocument
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,27 +1,95 @@
-// const rewire = require('rewire')
-// var index = rewire("./index.js")
-const index = require('./index.js')
+const qs = require('./index.js')
 
-test('no string entered', () => {
-  expect(index()).toBe(null)
+test('falsy/empty values entered', () => {
+  const vals = [null, false, 0, -0, 0n, undefined, NaN, '', [], {}]
+  vals.forEach(val => {
+    expect(qs(val)).toBe(null)
+    expect(qs`${val}`).toBe(null)
+    vals.forEach(val2 => {
+      expect(qs(val, val2)).toBe(null)
+    })
+  })
+  expect(qs``).toBe(null)
 })
 
-test('null entered', () => {
-  expect(index(null)).toBe(null)
+test('no value entered', () => {
+  expect(qs()).toBe(null)
+  expect(qs).toBeInstanceOf(Function)
 })
 
-test('false entered', () => {
-  expect(index(false)).toBe(null)
+test('element not found on page', () => {
+  document.body.innerHTML = '<div class="body-part"></div>'
+  expect(qs('#body-part')).toBeInstanceOf(Array)
+  expect(qs`#body-part`.length).toBe(0)
 })
 
-test('empty string entered', () => {
-  expect(index('')).toBe(null)
+test('element found on page', () => {
+  document.body.innerHTML = '<div class="body-part">Hello</div>'
+  const bodyPart = qs`.body-part`
+  expect(bodyPart).toBeInstanceOf(Array)
+  expect(bodyPart[0]).toBeInstanceOf(HTMLElement)
+  expect(bodyPart[0]).toBe(document.querySelector('.body-part'))
+  expect(bodyPart[0]).not.toBe(document.querySelector('#body-part'))
 })
 
-test('incorrect arguments entered', () => {
-  expect(index('', null)).toBe(null)
+test('set and get methods tested', () => {
+  document.body.innerHTML = '<div class="body-part">Hello</div>'
+  const bodyPart = qs`.body-part`
+  expect(bodyPart[0].innerHTML).toBe('Hello')
+  bodyPart.set(el => el.innerHTML = 'Hey now')
+  expect(bodyPart[0].innerHTML).toBe('Hey now')
+  expect(bodyPart.get(el => el.innerHTML)[0]).toBe('Hey now')
 })
 
-test('more incorrect arguments entered', () => {
-  expect(index('test')).toBe(null)
+test('only method tested', () => {
+  document.body.innerHTML = `
+    <div class="body-part">apple is red</div>
+    <div class="body-part">banana is yellow</div>
+  `
+  const bodyPart = qs`.body-part`.only(1)
+  expect(bodyPart[0].innerHTML).toBe('banana is yellow')
 })
+
+test('filter method tested', () => {
+  document.body.innerHTML = `
+    <div class="body-part">apple is red</div>
+    <div class="body-part">banana is yellow</div>
+    <div class="body-part">cherry is red</div>
+  `
+  const bodyPart = qs`.body-part`
+  expect(bodyPart.filter(el => el.innerHTML.includes('red')).length).toBe(2)
+  expect(bodyPart.filter(el => el.innerHTML.includes('red'))[0].innerHTML).toBe('apple is red')
+})
+
+test('adding event listener and testing event', () => {
+  document.body.innerHTML = `
+    <div class="body-part">Hello</div>
+    <img class="body-part" />
+  `
+  let testVal = false
+  const els = qs`.body-part`.click(ev => {
+    expect(els).toBeInstanceOf(Array)
+    expect(els.length).toBe(2)
+    testVal = true
+  })
+  clickEvent('.body-part', 'click')
+  expect(testVal).toBe(true)
+})
+
+test('removing event listener', () => {
+  document.body.innerHTML = '<div class="body-part">Hello</div>'
+  let testVal = false
+  const clickFunc = () => { console.log(clicked) }
+  qs`.body-part`.click(clickFunc)
+  qs`.body-part`.remove.click(clickFunc)
+  clickEvent('.body-part', 'click')
+  expect(testVal).toBe(false)
+})
+
+// create artificial event
+function clickEvent (selector, event) {
+  const bodyPart = document.querySelector(selector)
+  const evObj = document.createEvent('Events')
+  evObj.initEvent(event, true, false)
+  bodyPart.dispatchEvent(evObj)
+}
